8553409a4f68cbc2a8acae01e1d58fd2abd67958
RafaelRibeirodeCampos <rafael.campos77@etec.sp.gov.br>
2025-09-04 12:16:45 -0300

    Criado 2 métodos construtores para a superclasse.

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,15 +1,16 @@
 
 public class ClasseAutomotores {
-
-	public ClasseAutomotores() {
 		String corAt;
 		String marcaAt;
 		String modeloAt;
 		String tipoAt;
-	}
 	public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
 		corPar = corAt;
 		marcaPar = marcaAt;
 		modeloPar = modeloAt;
+		tipoPar = tipoAt;
+	}
+	public ClasseAutomotores() {
 	
+	}
 }
